eec1c55b72ab8aff0657fc8bf0433ce05bca0c9f
Stefankow20p <76476591+N1ktt@users.noreply.github.com>
2021-12-29 17:35:21 +0100

    Added getting data about current inflation from OECD

--- script.js
+++ script.js
@@ -1,72 +1,56 @@
 let muzyczka = new Audio("Among_Us_Trap_Remix_Bass_Boosted_Leonz.mp3")
 let klakson = new Audio("clown_horn_-_sound_effect.mp3")
 const sasin = document.querySelector("img")
-// muzyczka.addEventListener(
-//     "ended",
-//     function () {
-//         this.currentTime = 0
-//         this.play()
-//     },
-//     false
-// )
-// muzyczka.play()
+muzyczka.addEventListener(
+    "ended",
+    function () {
+        this.currentTime = 0
+        this.play()
+    },
+    false
+)
+muzyczka.play()
 
 sasin.addEventListener("click", () => {
     klakson.play()
 })
-// async function getData() {
-//     let apiUrl = "https://www.statbureau.org/calculate-inflation-price-jsonp?jsoncallback=?"
-
-//     let country = "united-states"
-//     let start = "2012/1/1"
-//     let end = "2012/12/01"
-//     let amount = "100"
-//     let format = true
-//     const data = { country, start, end, amount, format }
-//     const options = {
-//         method: "POST",
-//         body: JSON.stringify(data),
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//     }
-//     const res = await fetch(apiUrl, options)
-//     const results = await res.json()
-//     console.log(results)
-// }
-// getData()
-
-// $("#calc").on("click", function calculate() {
-//     $.getJSON(apiUrl, {
-//         country: "united-states",
-//         start: $("#startDate").val(),
-//         end: $("#endDate").val(),
-//         amount: $("#startPrice").val(),
-//         format: true,
-//     }).done(function (data) {
-//         $("#endPrice").val(data)
-//     })
-
-// })
 
 async function getData() {
-    let apiUrl = "https://www.statbureau.org/get-data-json?jsoncallback=?"
+    let today = new Date()
 
-    // let country = "united-states"
-    // let start = "2012/1/1"
-    // let end = "2012/12/01"
-    // let amount = "100"
-    // let format = false
-    // const data = { country }
-    // const options = {
-    //     country: "united-states",
-    // }
-    const res = await fetch(apiUrl, {
-        country: "united-states",
-    })
+    let todayYear = today.getFullYear()
+    let todayMonth = today.getMonth()
+    let prevMonth = todayMonth - 1
+    let prevYear = todayYear
+    if (prevMonth == -1) {
+        prevMonth = 11
+        prevYear -= 1
+    }
+    prevMonth += 1
+    todayMonth += 1
+
+    // url dla polski 2 miesiace
+    const apiUrl = `https://stats.oecd.org/SDMX-JSON/data/PRICES_CPI/POL.CPALTT01.CTGY.M/all?startTime=${prevYear}-${prevMonth}&endTime=${todayYear}-${todayMonth}&dimensionAtObservation=allDimensions`
+
+    const res = await fetch(apiUrl)
     const results = await res.json()
-    console.log(results)
+    let span = document.querySelector("span")
+    let inflation = 0
+    if (results.dataSets[0].observations["0:0:0:0:1"] == undefined) {
+        // console.log(results.dataSets[0].observations["0:0:0:0:0"][0])
+        inflation = results.dataSets[0].observations["0:0:0:0:0"][0]
+    } else {
+        // console.log(results.dataSets[0].observations["0:0:0:0:1"][0])
+        inflation = results.dataSets[0].observations["0:0:0:0:1"][0]
+    }
+    inflation *= 10
+    inflation = Math.round(inflation)
+    inflation /= 10
+    span.innerHTML = inflation
 }
 getData().catch((error) => {
+    console.error("Data recieved")
     console.error(error)
 })
+
+// data from https://www.oecd.org/
